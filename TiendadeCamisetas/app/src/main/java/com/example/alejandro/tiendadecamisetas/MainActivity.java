e1768e364a73912a52567d61c4083db5d7533a40
damacosta <2dam.acosta@institutoserpis.org>
2018-02-11 17:40:12 +0100

    Añadido Proyecto Final con error

---
+++ TiendadeCamisetas/app/src/main/java/com/example/alejandro/tiendadecamisetas/MainActivity.java
@@ -0,0 +1,189 @@
+package com.example.alejandro.tiendadecamisetas;
+
+import android.app.ListActivity;
+import android.database.sqlite.SQLiteDatabase;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.app.*;
+import android.app.Fragment;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends ListActivity {
+
+    public static final int NEW_ITEM = 1;
+    public static final int EDIT_ITEM = 2;
+    public static final int SHOW_ITEM = 3;
+
+    //elemento seleccionado
+    private int mLastRowSelected = 0;
+    public static DataBaseHelper mDbHelper = null;
+
+    int mStackPosition = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mDbHelper = new DataBaseHelper(this);
+        try {
+            fillData();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            showMessage(R.string.dataError);
+        }
+        registerForContextMenu(getListView());
+    }
+
+    private void showMessage(int message){
+        Context context = getApplicationContext();
+        CharSequence text = getResources().getString(message);
+        int duration = Toast.LENGTH_SHORT;
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+    }
+
+    protected void deleteEntry() {
+        try{
+            mDbHelper.open();
+            mDbHelper.delete(((ListEntry)getListAdapter().getItem(mLastRowSelected)).id);
+            mDbHelper.close();
+            fillData();
+        }catch (SQLException e){
+            e.printStackTrace();
+            showMessage(R.string.dataError);
+        }
+    }
+
+    private void fillData() {
+        // se abre la base de datos y se obtienen los elementos
+        mDbHelper.open();
+        Cursor itemCursor = mDbHelper.getItems();
+        ListEntry item = null;
+        ArrayList<ListEntry> resultList = new ArrayList<ListEntry>();
+        // se procesa el resultado
+        while (itemCursor.moveToNext()) {
+            int id = itemCursor.getInt(itemCursor.getColumnIndex(DataBaseHelper.SL_ID));
+            String nombre = itemCursor.getString(itemCursor.getColumnIndex(DataBaseHelper.SL_NOMBRE));
+            int precio = itemCursor.getInt(itemCursor.getColumnIndex(DataBaseHelper.SL_PRECIO));
+            item = new ListEntry();
+            item.id = id;
+            item.nombre = nombre;
+            item.precio= precio;
+            resultList.add(item);
+        }
+        //cerramos la base de datos
+        itemCursor.close();
+        mDbHelper.close();
+        //se genera el adaptador
+        TaskAdapter items = new TaskAdapter(this, R.layout.row_list, resultList, getLayoutInflater());
+        //asignar adaptador a la lista
+        setListAdapter(items);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.new_item:
+                Intent intent = new Intent (this,ItemActivity.class);
+                startActivityForResult(intent, NEW_ITEM);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("position",mStackPosition);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == EDIT_ITEM || requestCode == NEW_ITEM){
+            if (resultCode == Activity.RESULT_OK){
+                try {
+                    fillData();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                    showMessage(R.string.dataError);
+                }
+            }
+        }
+    }
+
+    private class TaskAdapter extends ArrayAdapter<ListEntry> {
+        private LayoutInflater mInflater;
+        private List<ListEntry> mObjects;
+
+        private TaskAdapter(Context context, int resource, List<ListEntry> objects, LayoutInflater mInflater) {
+            super(context, resource, objects);
+            this.mInflater = mInflater;
+            this.mObjects = objects;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ListEntry listEntry = mObjects.get(position);
+            // obtención de la vista de la línea de la tabla
+            View row = mInflater.inflate(R.layout.row_list, null);
+            //rellenamos datos
+            TextView nombre = (TextView) row.findViewById(R.id.row_name);
+            TextView precio = (TextView) row.findViewById(R.id.row_price);
+            nombre.setText(listEntry.nombre);
+            precio.setText(listEntry.precio);
+
+            // dependiendo de la importancia, se muestran distintos iconos
+            ImageView icon = (ImageView) row.findViewById(R.id.row_team);
+            icon.setTag(listEntry.id);
+            switch (listEntry.id) {
+                case 1:
+                    icon.setImageResource(R.drawable.barcelona);
+                    break;
+                case 2:
+                    icon.setImageResource(R.drawable.madrid);
+                    break;
+                default:
+                    icon.setImageResource(R.color.colorAccent);
+                    break;
+            }
+            return row;
+        }
+    }
+    private class ListEntry {
+        int id;
+        String nombre;
+        int precio;
+    }
+}
